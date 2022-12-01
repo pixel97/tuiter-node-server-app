f547f33eafe3c019e71447357faacdf6d34fb3f6
Anushka Jain <anushka.dkj@gmail.com>
2022-11-30 19:15:24 -0500

    Added changes

--- controller/tuits/tuits-schema.js
+++ controller/tuits/tuits-schema.js
@@ -4,6 +4,16 @@ const schema = mongoose.Schema(
     tuit: String,
     likes: Number,
     liked: Boolean,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number,
+    title: String,
+    topic: String,
+    username: String,
+    handle: String,
+    time: String,
+    image: String,
+    icon: String,
   },
   { collection: "tuits" }
 );
